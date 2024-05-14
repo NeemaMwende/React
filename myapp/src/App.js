3efbca8f6f5078235f3ba2f86be723818c07fd30
NeemaMwende <gracepete12@gmail.com>
2024-05-14 23:04:23 +0300

    updates

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -16,6 +16,7 @@
 
 import { useState } from "react";
 import "./App.css";
+import { validateEmail } from "./utils";
 // import DessertsList from "./DessertsList";
 // import MyForm from "./MyForm";
 
@@ -46,7 +47,7 @@ import "./App.css";
 //   const itemLists = `${name} - ${calories.sort(a,b)}` 
 // })
 
-function App() {
+// function App() {
 //   const [name, setName] = useState("");
 //   const [score, setScore] = useState("10");
 //   const [comment, setComment] = useState("");
@@ -68,8 +69,8 @@ function App() {
   //     setScore("10");
   // }
 
-  return (
-    <div className="App">
+  // return (
+  //   <div className="App">
       {/* <h2>List of low calorie desserts:</h2>
       <DessertsList data={desserts} /> */}
 
@@ -103,6 +104,87 @@ function App() {
           <button type="submit">Submit</button>
         </fieldset>
       </form> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//NEW CODE
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => {
+    // Implement this function
+    return true;
+  };
+
+  const clearForm = () => {
+    // Implement this function
+  };
+
+  const handleSubmit = () => {
+    alert("Account created!");
+    clearForm();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input placeholder="First name" />
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input placeholder="Last name" />
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            <input placeholder="Email address" />
+          </div>
+          <div className="Field">
+            <label>
+              Password <sup>*</sup>
+            </label>
+            <input placeholder="Password" />
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 }
