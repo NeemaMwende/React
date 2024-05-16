fb1eb1e8322003ca6ab932ebb61c8d123de7a34a
NeemaMwende <gracepete12@gmail.com>
2024-05-16 20:46:30 +0300

    testing the feedback form

--- myapp/src/App.test.js
+++ myapp/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+// import App from './App';
+import FeedbackForm from "./FeedbackForm"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("Feedback Form", () => {
+  test("Submission is disabled if score is lower than 5 and there is no feedback", () => {
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: "4" } });
+
+    const SubmitButton = screen.getByRole("button");
+    fireEvent.click(SubmitButton);
+
+    expect(handleSubmit).not.toHaveBeenCalled();
+    expect(SubmitButton).toHaveAttributes("disabled");
+  });
 });
