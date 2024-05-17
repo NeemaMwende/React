309d18780a05c3f8126426b673d8d2837468c459
NeemaMwende <gracepete12@gmail.com>
2024-05-17 14:30:44 +0300

    updates

--- myapp/src/App.test.js
+++ myapp/src/App.test.js
@@ -1,25 +1,58 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-// import App from './App';
-import FeedbackForm from "./FeedbackForm"
+// import { fireEvent, render, screen } from '@testing-library/react';
+// // import App from './App';
+// import FeedbackForm from "./FeedbackForm"
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
+// // test('renders learn react link', () => {
+// //   render(<App />);
+// //   const linkElement = screen.getByText(/learn react/i);
+// //   expect(linkElement).toBeInTheDocument();
+// // });
+
+// describe("Feedback Form", () => {
+//   test("Submission is disabled if score is lower than 5 and there is no feedback", () => {
+//     const handleSubmit = jest.fn();
+//     render(<FeedbackForm onSubmit={handleSubmit} />);
+
+//     const rangeInput = screen.getByLabelText(/Score:/);
+//     fireEvent.change(rangeInput, { target: { value: "4" } });
+
+//     const SubmitButton = screen.getByRole("button");
+//     fireEvent.click(SubmitButton);
+
+//     expect(handleSubmit).not.toHaveBeenCalled();
+//     expect(SubmitButton).toHaveAttributes("disabled");
+//   });
 // });
 
+import { fireEvent, render, screen } from "@testing-library/react";
+import FeedbackForm from "./FeedbackForm";
+
 describe("Feedback Form", () => {
-  test("Submission is disabled if score is lower than 5 and there is no feedback", () => {
+  test("User is able to submit the form if the score is lower than 5 and additional feedback is provided", () => {
+    const score = "3";
+    const comment = "The pizza crust was too thick";
     const handleSubmit = jest.fn();
     render(<FeedbackForm onSubmit={handleSubmit} />);
 
-    const rangeInput = screen.getByLabelText(/Score:/);
-    fireEvent.change(rangeInput, { target: { value: "4" } });
+    // You have to write the rest of the test below to make the assertion pass
 
-    const SubmitButton = screen.getByRole("button");
-    fireEvent.click(SubmitButton);
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment,
+    });
+  });
 
-    expect(handleSubmit).not.toHaveBeenCalled();
-    expect(SubmitButton).toHaveAttributes("disabled");
+  test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
+    const score = "9";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // You have to write the rest of the test below to make the assertion pass
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment: "",
+    });
   });
 });
+
---
+++ myapp/src/FeedbackFoorm.js
@@ -0,0 +1,99 @@
+// // import { useEffect , useState} from "react";
+// // import DessertsList from "./DessertsList"
+// // import MyForm from "./MyForm";
+
+// import { useState } from "react";
+// import "./App.css";
+// import { validateEmail } from "./utils";
+// import DessertsList from "./DessertsList";
+// import MyForm from "./MyForm";
+
+// const desserts = [
+//   {
+//     name: "Chocolate Cake",
+//     calories: 400,
+//     createdAt: "2022-09-01",
+//   },
+//   {
+//     name: "Ice Cream",
+//     calories: 200,
+//     createdAt: "2022-01-02",
+//   },
+//   {
+//     name: "Tiramisu",
+//     calories: 300,
+//     createdAt: "2021-10-03",
+//   },
+//   {
+//     name: "Cheesecake",
+//     calories: 600,
+//     createdAt: "2022-01-04",
+//   },
+// ];
+
+// const Lists = DessertsList.map(desserts => {
+//   const itemLists = `${name} - ${calories.sort(a,b)}` 
+// })
+
+// function FeedbackForm() {
+//       const [name, setName] = useState("");
+//       const [score, setScore] = useState("10");
+//       const [comment, setComment] = useState("");
+    
+//     //   const handleSubmit = (e) => {
+//     //     e.preventDefault();
+//     //     setName("");
+//     //     console.log("form submitted");
+    
+    
+//       const handleSubmit = (e) => {
+//         e.preventDefault();
+//         if (Number(score) <= 5 && comment.length <=10) 
+//           {
+//             alert("Please provide a comment explaining why the experience");
+//           }
+//           console.log("Form submitted!");
+//           setComment("");
+//           setScore("10");
+//       }
+    
+//        return (
+//          <div className="App">
+//            <h2>List of low calorie desserts:</h2>
+//           <DessertsList data={desserts} /> 
+    
+//           {/* //uncontrolled form  */}
+//            <MyForm />
+//           <h2>List of low calorie desserts:</h2>
+//           <DessertsList data={desserts} />
+//           <form onSubmit={handleSubmit}> 
+//             <fieldset>
+//               <div className="field">
+//                 <label htmlFor="name">Name:</label>
+//                 <input id="name" type="text" placeholder="Name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+//               </div>
+//               <button disabled={!name} type="submit">Submit</button>
+//             </fieldset>
+//           </form> 
+    
+//          <h2>Please give us a review </h2>
+//         <h4>Your Welcome</h4>
+//           <form onSubmit={handleSubmit}>
+//             <fieldset>
+//               <h2>Feedback Form</h2>
+//               <div className="Field">
+//                 <label>Score: {score}</label>
+//                 <input type="range" min={0} max={10} value={score} onChange={e => setScore(e.target.value)}/>
+//               </div>
+//               <div className="Field">
+//                 <label htmlFor="comment">Comment</label>
+//                 <textarea value={comment} onChange={e => setComment(e.target.value)} />
+//               </div>
+//               <button type="submit">Submit</button>
+//             </fieldset>
+//           </form> 
+//          </div>
+//        );
+//      }
+    
+//      export default FeedbackForm;
